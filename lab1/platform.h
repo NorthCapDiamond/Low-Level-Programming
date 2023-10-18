1982297741965b704064835417b2d45d37888a76
Drobysh Dmitry <90309201+NorthCapDiamond@users.noreply.github.com>
2023-10-18 19:45:17 +0300

    Delete lab1 directory

--- lab1/platform.h
+++
@@ -1,8 +0,0 @@
-#if defined(__APPLE__) || defined(__MACH__) || defined(__unix__) || defined(__unix)
-
-#include<unistd.h>
-#include<sys/types.h>
-
-#define ftrunc(fd, len) ftruncate(fd, len)
-
-#endif
